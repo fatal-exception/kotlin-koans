2cbdefa24b482bed3e760a1f4ddbff81af90a05e
Matthew Ralph <ralpmat@gmail.com>
2018-05-16 17:21:47 +0100

    Did up to 37

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -4,9 +4,9 @@ import util.TODO
 import java.util.*
 
 fun buildStringExample(): String {
-    fun buildString(build: StringBuilder.() -> Unit): String {
+    fun buildString(buildme: StringBuilder.() -> Unit): String {
         val stringBuilder = StringBuilder()
-        stringBuilder.build()
+        stringBuilder.buildme()
         return stringBuilder.toString()
     }
 
@@ -29,11 +29,16 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    fun buildMap(build: MutableMap<Int, String>.() -> Unit): Map<Int, String> {
+        val m = HashMap<Int, String>()
+        m.build()
+        return m
+    }
+
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
