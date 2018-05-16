4aed4e81edfb7d0bd1b901c506b7baf165976fa1
Matthew Ralph <ralpmat@gmail.com>
2018-05-16 14:17:11 +0100

    Did koan 30

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,14 +1,13 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
-  : Comparable<MyDate> {
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
     override operator fun compareTo(other: MyDate): Int {
       return if (year != other.year) year - other.year
       else if (month != other.month) month - other.month
       else dayOfMonth - other.dayOfMonth
     }
     operator fun rangeTo(other: MyDate) = DateRange(this, other)
-  }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
@@ -17,6 +16,7 @@ operator fun MyDate.plus(ti: TimeInterval): MyDate =
 
 operator fun MyDate.plus(rti: RepeatedTimeInterval): MyDate =
   addTimeIntervals(rti.ti, rti.occurrences)
+
 
 enum class TimeInterval {
     DAY,
